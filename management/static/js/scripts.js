3afcd9e70e5e0088f372cc7c29d332f587d83975
Sama Mansour <samamansour306@gmail.com>
2022-04-25 21:04:26 +0300

    load more

--- management/static/js/scripts.js
+++ management/static/js/scripts.js
@@ -11,3 +11,19 @@ function toggleMenu() {
   menuToggle.classList.toggle("active");
   menu.classList.toggle("active");
 }
+
+function loadMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
