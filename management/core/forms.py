58c4d2ca242f9d6cc71b6f4d1d26eb2411528bde
Sama Mansour <samamansour306@gmail.com>
2022-05-11 23:52:25 +0300

    add phone number

--- management/core/forms.py
+++ management/core/forms.py
@@ -4,14 +4,16 @@ from django.contrib.auth.models import User
 
 
 
+
 # Create your forms here.
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
+	phone = forms.CharField(required=True)
 
 	class Meta:
 		model = User
-		fields = ("username", "email", "password1", "password2")
+		fields = ("username", "email", "phone", "password1", "password2")
 
 	def save(self, commit=True):
 		user = super(NewUserForm, self).save(commit=False)
--- management/management/urls.py
+++ management/management/urls.py
@@ -23,7 +23,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView),
-    path('dashboard/', DashboardView),
     path('signup/', register_request,name="register"),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path("logout", auth_views.LogoutView.as_view(template_name='accounts\logout.html'), name="logout"),
